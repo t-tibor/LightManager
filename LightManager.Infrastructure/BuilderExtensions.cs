cd23890f104331de1d1ecc4fb3d940fa4c7c10d8
Tibor Tusori <tibor.tusori@zeiss.com>
2025-04-28 17:42:02 +0200

    Basic light on/off ui.

---
+++ LightManager.Infrastructure/BuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using LightManager.Infrastructure.MQTT;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LightManager.Infrastructure;
+
+public static class BuilderExtensions
+{
+	public static IHostApplicationBuilder AddMqttInfrastructure(this IHostApplicationBuilder builder)
+	{
+		builder.Services.Configure<MqttConfig>(builder.Configuration.GetSection(nameof(MqttConfig)));
+		builder.Services.AddSingleton<IMqttConnector, MqttConnector>();
+		return builder;
+	}
+}
---
+++ LightManager.PanelUi/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LightManager.PanelUi.Pages;
+public class IndexModel : PageModel
+{
+	public void OnGet()
+	{
+		// This method is intentionally left empty.
+		// You can add any logic you want to execute when the page is accessed.
+	}
+}
